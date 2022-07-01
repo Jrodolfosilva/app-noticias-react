f392c9cc6c62b2479937d6c33b4c3f42cef053b5
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-06-30 21:26:50 -0300

    Função dele postagem
    Criei a função de deletar notícias do backend (API). usei o useParams para capturá o id e fazer um DELETE. Implementei também condições para renderizar o titulo da pagina, "cadastro" ou "edição" e para o value do input submit.

--- news/src/form/Form.js
+++ news/src/form/Form.js
@@ -1,9 +1,14 @@
 import React,{useEffect,useState} from "react"
+import {useParams} from "react-router-dom"
 import Styles from "./Form.module.css"
 import axios from "axios"
 
 
 const Form = ()=>{
+
+    const {id} = useParams()
+
+   
 
     const noticiasInit = {
         id:0,
@@ -17,23 +22,36 @@ const Form = ()=>{
         const {name,value} = e.target
         setNoticias({...noticias,[name]:value})
     }
+
     const Submit = (e)=>{
         e.preventDefault()
+        
+        /*let method = "post" 
+        id?id=Number.parseInt(id):id=null
+        if(id) method = "put"
+        */
+        
         axios.post("http://localhost:5000/materias",noticias).then((response)=>{
             alert('cadastrado com sucesoo')
         }).catch(()=>{
             alert('erroo')
         })
     } 
-    
+    const Delete = ()=>{
+        axios.delete(`http://localhost:5000/materias/${id}`).then((resp)=>{
+            alert('Excluido com sucesso')
+        })
+    }
+  
     return(
         <div className={Styles.container_form}>
-          <h2>Cadastro</h2>
+          {id?<h2>Edição</h2>:<h2>Cadastro</h2>}
             <form>
                 <input type="text" name="title" placeholder="Digite o título da notícia" onChange={Onchanges} />
                 <input type="text" name="autor" placeholder="Digite o nome do Autor" onChange={Onchanges} />
                 <input type="text" name="content" placeholder="Notícia" onChange={Onchanges} />
-                <input type="submit" value="Publicar" onClick={Submit}/>
+                <input type="submit" value={id?"Atualizar":"Publicar"} onClick={Submit}/>
+                {id&&<button onClick={Delete}>Excluir</button>}
             </form>
         </div>
     );
